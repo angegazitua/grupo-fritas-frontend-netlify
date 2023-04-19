1fb2593b217b73d07c62badf6a793541ab01dfa9
angegazitua <ange.gazitua@uc.cl>
2023-04-19 17:15:56 -0400

    Listas fotos del board

--- War-of-Houses/src/game/Board.jsx
+++ War-of-Houses/src/game/Board.jsx
@@ -2,43 +2,52 @@
 // https://www.npmjs.com/package/react-hexgrid?activeTab=readme
 
 import { HexGrid, Layout, Hexagon, Text, Pattern, Path, Hex } from 'react-hexgrid';
-// import './board.css'
-import Image1 from '../assets/img/logo_howards.png'
+import './board.css'
+import Image1 from '../assets/img/bosque_prohibido.jpg'
+import Image2 from '../assets/img/cabaña_hagrid.jpg'
+import Image3 from '../assets/img/chamber.png'
+import Image4 from '../assets/img/diagonal_alley.jpg'
+import Image5 from '../assets/img/oficina_dumbledore.jpg'
+import Escoba from './Escoba.jsx'
 
 function Board () {
+    const hexagonSize = { x: 18, y: 10 };
+    const hexagonSize3 = { x: 10, y: 10 };
     return (
       <div id="board">
-        <HexGrid width={1400} height={800} viewBox="-50 -50 100 100">
+        <HexGrid width={1550} height={800} viewBox="-50 -50 100 100">
           {/* Grid with manually inserted hexagons */}
           <Layout size={{ x: 10, y: 10 }} flat={true} spacing={1.01} origin={{ x: 0, y: 0 }}>
-            <Hexagon q={0} r={0} s={0} fill = 'url(#pat-1)'/>
+            <Hexagon q={0} r={0} s={0} fill = 'pat-1'/>
             {/* Using pattern (defined below) to fill the hexagon */}
-            <Hexagon q={0} r={1} s={-1} className='hex'/>
-            <Hexagon q={1} r={0} s={-1} />
-            <Hexagon q={1} r={-1} s={0} />
-            <Hexagon q={0} r={-1} s={1} />
-            <Hexagon q={-1} r={0} s={1} />
-            <Hexagon q={-1} r={1} s={0} />
+            <Hexagon q={0} r={1} s={-1} fill = 'pat-2'/>
+            <Hexagon q={1} r={0} s={-1} fill = 'pat-3'/>
+            <Hexagon q={1} r={-1} s={0} fill = 'pat-4'/>
+            <Hexagon q={0} r={-1} s={1} fill = 'pat-5'/>
+            <Hexagon q={-1} r={0} s={1} fill = 'pat-1'/>
+            <Hexagon q={-1} r={1} s={0} fill = 'pat-2'/>
 
-            <Hexagon q={-2} r={0} s={1} />
-            <Hexagon q={0} r={2} s={-2} />
-            <Hexagon q={1} r={1} s={-2} />
-            <Hexagon q={2} r={0} s={-2} />
-            <Hexagon q={2} r={-1} s={-1} />
-            <Hexagon q={2} r={-2} s={0} />
-            <Hexagon q={1} r={-2} s={1} />
-            <Hexagon q={0} r={-2} s={2} />
-            <Hexagon q={-1} r={-1} s={2} />
-            <Hexagon q={-2} r={0} s={2} />
-            <Hexagon q={-2} r={1} s={1} />
-            <Hexagon q={-2} r={2} s={0} />
-            <Hexagon q={-1} r={2} s={-1} />
+            <Hexagon q={-2} r={0} s={1} fill = 'pat-3'/>
+            <Hexagon q={0} r={2} s={-2} fill = 'pat-4'/>
+            <Hexagon q={1} r={1} s={-2} fill = 'pat-5'/>
+            <Hexagon q={2} r={0} s={-2} fill = 'pat-1'/>
+            <Hexagon q={2} r={-1} s={-1} fill = 'pat-2' />
+            <Hexagon q={2} r={-2} s={0} fill = 'pat-3'/>
+            <Hexagon q={1} r={-2} s={1} fill = 'pat-4'/>
+            <Hexagon q={0} r={-2} s={2} fill = 'pat-5'/>
+            <Hexagon q={-1} r={-1} s={2} fill = 'pat-1'/>
+            <Hexagon q={-2} r={0} s={2} fill = 'pat-2'/>
+            <Hexagon q={-2} r={1} s={1} fill = 'pat-3'/>
+            <Hexagon q={-2} r={2} s={0} fill = 'pat-4'/>
+            <Hexagon q={-1} r={2} s={-1} fill = 'pat-5'/>
 
             {/* Pattern and text */}
-
           </Layout>
-          <Pattern id="pat-1" link={Image1} />
-          <Pattern id="pat-2" link="http://cat-picture2" />
+          <Pattern id="pat-1" link={Image1} size={hexagonSize}/>
+          <Pattern id="pat-2" link={Image2} size={hexagonSize}/>
+          <Pattern id="pat-3" link={Image3} size={hexagonSize3}/>
+          <Pattern id="pat-4" link={Image4} size={hexagonSize}/>
+          <Pattern id="pat-5" link={Image5} size={hexagonSize}/>
 
         </HexGrid>
       </div>
@@ -46,3 +55,4 @@ function Board () {
 }
 
 export default Board;
+
