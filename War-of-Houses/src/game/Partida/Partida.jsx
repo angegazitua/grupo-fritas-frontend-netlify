0a6df4fa163be485ac5da8c7aca2e46facf5152f
CAMILA BENNETT <camila.bennett@uc.cl>
2023-06-22 19:00:53 -0400

    avances en partida

--- War-of-Houses/src/game/Partida/Partida.jsx
+++ War-of-Houses/src/game/Partida/Partida.jsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useLocation } from "react-router-dom";
 // https://www.npmjs.com/package/react-hexgrid?activeTab=readme
 import React, {createContext, useState, useEffect} from "react";
 import { HexGrid, Layout, Hexagon, Text, Pattern, Path, Hex} from 'react-hexgrid';
@@ -15,12 +16,71 @@ import CartaPhoenix from '../../assets/img/pluma-pheonix.jpg';
 import CartaUnicornio from '../../assets/img/pelo-unicornio.jpg';
 import CartaVarita from '../../assets/img/varita-magica.jpg';
 import CartaSerpiente from '../../assets/img/diente-serpiente.jpg';
+import CabanaRoja from '../../assets/img/cabana-roja.png';
+// import CabanaVerde from '../../assets/img/cabana-verde.png';
+import CabanaAzul from '../../assets/img/cabana-azul.png';
+// import CabanaAmarilla from '../../assets/img/cabana-amarilla.png';
+import CastilloRojo from '../../assets/img/castillo-rojo.png';
+// import CastilloVerde from '../../assets/img/castillo-verde.png';
+import CastilloAzul from '../../assets/img/castillo-azul.png';
+// import CastilloAmarillo from '../../assets/img/castillo-amarillo.png';
 
 function Partida () {
     const hexagonSize = { x: 18, y: 10 };
     const hexagonSize3 = { x: 10, y: 10 };
 
-    
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    const idPartida = searchParams.get("idPartida");
+
+    let foto_pos = {'foto_pos_1_2_5': null, 'foto_pos_1_3_5': null, 'foto_pos_2_4_7': null,
+      'foto_pos_2_5_7': null, 'foto_pos_3_5_8': null, 'foto_pos_3_6_8': null,
+      'foto_pos_4_7_9': null, 'foto_pos_5_7_10': null, 'foto_pos_5_8_10': null,
+      'foto_pos_6_8_11': null, 'foto_pos_7_9_12': null, 'foto_pos_7_10_12': null,
+      'foto_pos_8_10_13': null, 'foto_pos_8_11_13': null, 'foto_pos_9_12_14': null,
+      'foto_pos_10_12_15': null, 'foto_pos_10_13_15': null, 'foto_pos_11_13_16': null,
+      'foto_pos_12_14_17': null, 'foto_pos_12_15_17': null, 'foto_pos_13_15_18': null,
+      'foto_pos_13_16_18': null, 'foto_pos_15_17_19': null, 'foto_pos_15_18_19': null
+  };
+
+    useEffect(() => {
+      cargarPartida();
+      // Hacer que cambie cada vez que cambie jugador actual
+    }, []);
+  
+    const cargarPartida = () => {
+      axios
+        .get(`${import.meta.env.VITE_BACKEND_URL}/estado_partida/${idPartida}`)
+        .then((response) => {
+          console.log(response.data);
+          const cabanas_rojas = response.data["jugador_1"]["cabanas"];
+          console.log(cabanas_rojas);
+          Object.keys(cabanas_rojas).forEach((key) => {
+            const pos = cabanas_rojas[key]["posicion"].split(",").join("_");
+            console.log(pos);
+            foto_pos[`foto_pos_${pos}`] = "pat-cabana-roja";
+            // poner en hexagono como fill = {foto_pos['foto_pos_1_2_5']}
+          });
+
+
+
+          const cabanas_verdes = response.data["jugador_2"]["cabanas"];
+          const cabanas_azules = response.data["jugador_3"]["cabanas"];
+          const cabanas_amarillas = response.data["jugador_4"]["cabanas"];
+
+          const castillos_rojos = response.data["jugador_1"]["castillos"];
+          const castillos_verdes = response.data["jugador_2"]["castillos"];
+          const castillos_azules = response.data["jugador_3"]["castillos"];
+          const castillos_amarillos = response.data["jugador_4"]["castillos"];
+
+
+          const foto_pos_1_2_5 = 'pat-castillo-rojo';
+
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
 
     return (
     
@@ -106,7 +166,7 @@ function Partida () {
 
             <Layout size={{ x: 3, y: 3 }} flat={true} spacing={1.01} origin={{ x: 0, y: 0 }}>
                 {/* Segunda vuelta */}
-                <Hexagon q={1} r={-2} s={1} className = 'hexagon_125' fill = 'pat-logo' onClick={() => alert("hola")}>
+                <Hexagon q={1} r={-2} s={1} className = 'hexagon_125' fill = 'pat-cabana-roja' onClick={() => alert("hola")}>
                 </Hexagon>
                 <Hexagon q={2} r={-1} s={-1} fill = 'pat-logo'>
                 </Hexagon>
@@ -175,13 +235,26 @@ function Partida () {
               
             </Layout>
 
+            {/* Patterns de terreno */}
             <Pattern id="pat-1" link={Image1} size={hexagonSize}/>
             <Pattern id="pat-2" link={Image2} size={hexagonSize}/>
             <Pattern id="pat-3" link={Image3} size={hexagonSize3}/>
             <Pattern id="pat-4" link={Image4} size={hexagonSize}/>
             <Pattern id="pat-5" link={Image5} size={hexagonSize}/>
-            <Pattern id="pat-cabaña" link={Image6}/>
             <Pattern id="pat-logo" link={Image7}/>
+
+            {/* Patterns de cabanas */}
+            <Pattern id="pat-cabana-roja" link={CabanaRoja} size={{x:1.8, y:1.8}}/>
+            {/* <Pattern id="pat-cabana-verde" link={CabanaVerde} size={{x:1.8, y:1.8}}/> */}
+            <Pattern id="pat-cabana-azul" link={CabanaAzul} size={{x:1.8, y:1.8}}/>
+            {/* <Pattern id="pat-cabana-amarilla" link={CabanaAmarilla} size={{x:1.8, y:1.8}}/> */}
+
+            {/* Patterns de castillos */}
+            <Pattern id="pat-castillo-rojo" link={CastilloRojo} size={{x:1.8, y:1.8}}/>
+            {/* <Pattern id="pat-castillo-verde" link={CastilloVerde} size={{x:1.8, y:1.8}}/> */}
+            <Pattern id="pat-castillo-azul" link={CastilloAzul} size={{x:1.8, y:1.8}}/>
+            {/* <Pattern id="pat-castillo-amarillo" link={CastilloAmarillo} size={{x:1.8, y:1.8}}/> */}
+
 
           </HexGrid>
           
